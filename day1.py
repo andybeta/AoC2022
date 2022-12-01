961a9fdf99f3cb476ca973e0dd2f5a407ba25e02
andybeta <savidgea@gmail.com>
2022-12-01 09:24:40 +0000

    Update day1.py
    refactor and tidy a bit

--- day1.py
+++ day1.py
@@ -9,15 +9,13 @@ totals=[]
 this_elf=0
 
 for cal in cals: # for each row in the input
-    if not cal:  # if it's blank then
+    if cal:  # if it's not blank then
+        this_elf+=int(cal) # add the value to this elf's total
+    else:
         totals.append(this_elf) # add the total for this elf to the totals list
         this_elf=0 # init this_elf ready for the next one
         continue   # get the next row
-    this_elf+=int(cal) # otherwise add the value to this elf's total
-totals.append(this_elf) # catch the last elf!
 totals.sort(reverse=True) # sort largest first
 
 print("Part 1:"+str(totals[0])) # print the single largest
-
-top3=totals[0]+totals[1]+totals[2] # sum the top 3
-print("Part 2:"+str(top3)) # print the total of the top 3
+print("Part 2:"+str(sum(totals[:3])))
